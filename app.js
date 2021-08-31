ac6478bc0166c4ccea883746c3af876011fab9c3
Shishir <shishirs520@gmail.com>
2021-08-31 22:19:31 +0545

    calculate profit or loss and show to user done

--- app.js
+++ app.js
@@ -15,16 +15,60 @@ function checkBtnHandler() {
   if ((initialPriceInput.value !== "") && (stocksQtyInput.value !== "") && (currentPriceInput.value !== "")) {
     if ((Math.sign(initialPrice) === 1) && (Math.sign(stocksQty) === 1) && (Math.sign(currentPrice) === 1)) {
       calculateProfitOrLoss(initialPrice, stocksQty, currentPrice);
-    } else if ((Math.sign(initialPrice) === -1) || (Math.sign(stocksQty) === -1) || (Math.sign(currentPrice) === -1)) {
+    }
+    else if ((Math.sign(initialPrice) === -1) || (Math.sign(stocksQty) === -1) || (Math.sign(currentPrice) === -1)) {
       showMessageText("Inputs can't be negatives. Please enter positive numbers in all fields.");
-    } else if ((Math.sign(initialPrice) === 0) || (Math.sign(stocksQty) === 0) || (Math.sign(currentPrice) === 0)) {
+    }
+    else if ((Math.sign(initialPrice) === 0) || (Math.sign(stocksQty) === 0) || (Math.sign(currentPrice) === 0)) {
       showMessageText("Inputs can't be zeroes. Please enter positive numbers in all fields.");
     }
-  } else if ((initialPriceInput.value === "") && (stocksQtyInput.value === "") && (currentPriceInput.value === "")) {
+  }
+  else if ((initialPriceInput.value === "") && (stocksQtyInput.value === "") && (currentPriceInput.value === "")) {
     showMessageText("Enter all the three inputs given above. None should be empty.");
-  } else if ((initialPriceInput.value === "") || (stocksQtyInput.value === "") || (currentPriceInput.value === "")) {
+  }
+  else if ((initialPriceInput.value === "") || (stocksQtyInput.value === "") || (currentPriceInput.value === "")) {
     showMessageText("At least one out of three inputs is missing. Please enter all amounts.");
   }
+}
+
+function calculateProfitOrLoss(initialPrice, stocksQty, currentPrice) {
+  if (currentPrice > initialPrice) {
+    // profit logic
+    var profit = currentPrice - initialPrice;
+    var profitPercentage = (profit / initialPrice) * 100;
+    console.log("Profit:", profit, "and Profit Percentage:", profitPercentage);
+
+    outputText.style.display = "block";
+    showProfit(profit, profitPercentage);
+  }
+  else if (currentPrice < initialPrice) {
+    // loss logic
+    var loss = initialPrice - currentPrice;
+    var lossPercentage = (loss / initialPrice) * 100;
+    console.log("Loss:", loss, "and Loss Percentage:", lossPercentage);
+
+    outputText.style.display = "block";
+    showLoss(loss, lossPercentage);
+  }
+  else if (currentPrice === initialPrice) {
+    console.log("No profit or loss. The current price is the same as when you bought it.");
+
+    outputText.style.display = "block";
+    outputText.style.color = "black";
+    outputText.innerHTML = "No profit or loss. The current price is the same as when you bought it.";
+  }
+}
+
+// show profit to user
+function showProfit(profit, profitPercentage) {
+  outputText.style.color = "green";
+  outputText.innerHTML = `The profit is Rs. ${profit} and profit percentage is ${profitPercentage}%.`;
+}
+
+// show loss to user
+function showLoss(loss, lossPercentage) {
+  outputText.style.color = "red";
+  outputText.innerHTML = `The loss is Rs. ${loss} and loss percentage is ${lossPercentage}%.`;
 }
 
 function showMessageText(msg) {
